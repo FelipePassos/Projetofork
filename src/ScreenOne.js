51e95b9e62c551f021840b495af9304934c061b8
joao felipe <jfelipepassos@gmail.com>
2023-06-27 18:48:10 -0300

    fix: path, import and compilation errors. The project came and went from Disney

--- src/ScreenOne.js
+++ src/ScreenOne.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import { View, Image } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import AnotherScreenOne from './AnotherScreenOne';
+// import AnotherScreenOne from './AnotherScreenOne'; deve ta importando da Disney, pq desse projeto nao é
 import ScreenLogin from './ScreenLogin';
 
 const Stack = createNativeStackNavigator();
@@ -29,7 +29,7 @@ const App = () => {
 const AnotherScreenOne = () => {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Image source={require('./logo.png')} />
+      <Image source={require('../assets/Logo.png')} />
     </View>
   );
 };
